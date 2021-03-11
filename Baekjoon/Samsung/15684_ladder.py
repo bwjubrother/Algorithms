33fb8071afc6f2990c6c9731cedd27e2bdc4918b
bwjubrother <bwjubrother@gmail.com>
2021-03-12 00:37:46 +0900

    0312_Todo

---
+++ Baekjoon/Samsung/15684_ladder.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdin = open('15684.txt', 'r')
+
+n, m, h = map(int, input().split())
+ladder = [list(map(int, input().split() for _ in range(m)]
+
+# cnt > 3 and impossible , print(-1)
